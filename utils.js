23ab3b43ccd720abfbdf75fd8bf91959fa6bff49
Jackson <jackson.hardaker@gmail.com>
2018-08-22 12:37:13 -0400

    Add logging utils

--- utils.js
+++ utils.js
@@ -1,4 +1,24 @@
 const Slugify = require('slugify');
+const colors = {
+  Reset: '\x1b[0m',
+  Bright: '\x1b[1m',
+  Dim: '\x1b[2m',
+  Underscore: '\x1b[4m',
+  Blink: '\x1b[5m',
+  Reverse: '\x1b[7m',
+  Hidden: '\x1b[8m',
+  fg: {
+    Black: '\x1b[30m',
+    Red: '\x1b[31m',
+    Green: '\x1b[32m',
+    Yellow: '\x1b[33m',
+    Blue: '\x1b[34m',
+    Magenta: '\x1b[35m',
+    Cyan: '\x1b[36m',
+    White: '\x1b[37m',
+    Crimson: '\x1b[38m'
+  }
+};
 
 function slugify(text) {
   return Slugify(text, {
@@ -8,6 +28,35 @@ function slugify(text) {
   });
 }
 
+function log(f, args) {
+  const paramNames = _getParamNames(f);
+  const paramNameValues = paramNames.map((argName, index) => {
+    return {
+      [argName]: args[index],
+      toString: () => { return ` ${argName}: ${args[index]}` }
+    };
+  });
+
+  //console.log('\x1b[36m', 'sometext' ,'\x1b[0m');
+
+  console.log(
+    `[Calling Function ${colors.fg.Cyan}'${f.name}'${colors.Reset} with parameters:${colors.fg.Magenta}${paramNameValues}${colors.Reset}]`
+  );
+}
+
+function _getParamNames(f) {
+  const STRIP_COMMENTS = /(\/\/.*$)|(\/\*[\s\S]*?\*\/)|(\s*=[^,\)]*(('(?:\\'|[^'\r\n])*')|("(?:\\"|[^"\r\n])*"))|(\s*=[^,\)]*))/gm;
+  const ARGUMENT_NAMES = /([^\s,]+)/g;
+
+  var fnStr = f.toString().replace(STRIP_COMMENTS, '');
+  var result = fnStr
+    .slice(fnStr.indexOf('(') + 1, fnStr.indexOf(')'))
+    .match(ARGUMENT_NAMES);
+  if (result === null) result = [];
+  return result;
+}
+
 module.exports = {
-  slugify
+  slugify,
+  log
 };
